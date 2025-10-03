bcbaa2122f4a4b2c21248c46fc1a368f58c228fa
eya-amri <eya.amri@ensi-uma.tn>
2025-10-03 11:29:58 +0100

    my second commit

--- src/application/Test.java
+++ src/application/Test.java
@@ -1,6 +1,8 @@
 package application;
+
 public class Test {
     public static void main(String[] args) throws Exception {
+        // update code
         System.out.println("Hello, World!");
     }
 }
