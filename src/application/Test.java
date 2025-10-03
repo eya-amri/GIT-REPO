08f39ca3c0703be738848ae0b549277be1e45ce5
eya-amri <eya.amri@ensi-uma.tn>
2025-10-03 11:48:02 +0100

    third commit

--- src/application/Test.java
+++ src/application/Test.java
@@ -3,6 +3,6 @@ package application;
 public class Test {
     public static void main(String[] args) throws Exception {
         // update code
-        System.out.println("Hello, World!");
+        System.out.println("Je suis le master");
     }
 }
